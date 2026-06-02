532af9b82a0f60484d5e1c03a787bd64a3dba911
hailey <hnmorgan@calpoly.edu>
2026-06-02 15:20:46 -0700

    Env helpers

--- Program.fs
+++ Program.fs
@@ -21,7 +21,30 @@ and ExprC =
 // A Binding pairs a name with the value bound to it.
 and Binding = Binding of string * Value
 
-// An Env (environment) is just a list of bindings.
+// An env is a list of bindings.
 type Env = Binding list
 
 let mtEnv : Env = []
+
+// extend-env : add one binding to the front of an environment.
+let extendEnv (newBind: Binding) (oldEnv: Env) : Env =
+    newBind :: oldEnv // :: is like cons
+
+// extend-env-many : add several param/value pairs to an environment at once.
+let extendEnvMany (parms: string list) (vals: Value list) (oldEnv: Env) : Env =
+    if List.length parms <> List.length vals then
+        failwith (sprintf "VEBG: wrong arity, expected %A params but got %A args" parms vals)
+    else
+        List.fold2
+            (fun (acc: Env) (param: string) (v: Value) -> extendEnv (Binding(param, v)) acc)
+            oldEnv
+            parms
+            vals
+
+// lookup : find the value bound to name, searching front to back.
+let rec lookup (name: string) (env: Env) : Value =
+    match env with
+    | [] -> failwith (sprintf "VEBG: unbound identifier %s" name)
+    | Binding(n, v) :: rest ->
+        if name = n then v
+        else lookup name rest
