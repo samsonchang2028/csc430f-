5111939697a093a58e852bdf7c094d982c0bb280
AlexBloomer <alexbloomer@gmail.com>
2026-06-03 22:16:00 -0700

    added primitive application

--- Program.fs
+++ Program.fs
@@ -1,4 +1,4 @@
-// For more information see https://aka.ms/fsharp-console-apps
+﻿// For more information see https://aka.ms/fsharp-console-apps
 
 module Program
 
@@ -102,6 +102,71 @@ let rec containsDuplicate (lst: string list) : bool =
     match lst with
     | [] -> false
     | f :: r -> List.contains f r || containsDuplicate r
+let index_check (n : float) : bool =
+    n >= 0.0 && floor n = n
+
+// apply-primitive : Symbol, listof value -> value
+let apply_primitive op args =
+    match op with
+    | "+" ->
+        match args with 
+        | [NumV l; NumV r] -> NumV (l + r)
+        | _ -> failwith "VEBG: + expects two numbers"
+    | "-" ->
+        match args with 
+        | [NumV l; NumV r] -> NumV (l - r)
+        | _ -> failwith "VEBG: - expects two numbers"
+    | "*" ->
+        match args with 
+        | [NumV l; NumV r] -> NumV (l * r)
+        | _ -> failwith "VEBG: * expects two numbers"
+    | "/" ->
+        match args with 
+        | [NumV l; NumV r] -> 
+            if r = 0 then
+                failwith "VEBG: / divide by zero error"
+            else
+                NumV (l / r)
+        | _ -> failwith "VEBG: / expects two numbers"
+    | "<=" ->
+        match args with 
+        | [NumV l; NumV r] -> BoolV (l <= r)
+        | _ -> failwith "VEBG: <= expects two numbers"
+    | "substring" ->
+        match args with 
+        | [StrV s; NumV start; NumV stop] ->   
+            if not (index_check start) then
+                failwith "VEBG: substring expects natural number"
+            elif not (index_check stop) then
+                failwith "VEBG: substring expects natural number"
+            else
+                let startId = int start
+                let stopId = int stop 
+                if (startId > s.Length) then
+                    failwith "VEBG: substring start index out of range"
+                elif (stopId > s.Length) then
+                    failwith "VEBG: substring stop index out of range"
+                elif (startId > stopId) then
+                    failwith "VEBG: substring start index greater than stop index"
+                else
+                    (StrV (s.Substring(startId, stopId-startId)))
+        | _ -> failwith "VEBG: substring expects string and two numbers"
+    | "strlen" ->
+        match args with 
+        | [StrV s] -> NumV (float s.Length)
+        | _ -> failwith "VEBG: strlen expects one string"
+    | "equal?" -> 
+        match args with
+        | [NumV a; NumV b] -> BoolV (a=b)
+        | [BoolV a; BoolV b] -> BoolV (a=b)
+        | [StrV a; StrV b] -> BoolV (a=b)
+        | [_; _] -> BoolV false
+        | _ -> failwith "VEBG equal? expects exactly two arguments"
+    | "error" -> 
+        match args with
+        |[v] -> failwith ("VEBG User Error" + string v)
+        | _ -> failwith "VEBG error expected exactly one argument"
+    | _ -> failwith "VEBG: unknown primitive operator"
 
 // INTERPRETER
 // interp : ExprC -> Env -> Value
@@ -121,6 +186,7 @@ let rec interp e env =
         let argVs = args |> List.map (fun arg -> interp arg env)
         match funV with
         | CloV (parms, body, savedEnv) -> interp body (extendEnvMany parms argVs savedEnv)
+        | PrimV op -> apply_primitive op argVs
         | _ -> failwith "application of non-function"
 
 // TESTS
@@ -153,5 +219,92 @@ let unboundRaised =
     with _ -> true
 
 assert unboundRaised
+
+assert (interp (AppC (IdC "+", [NumC 1.0; NumC 2.0])) topEnv = NumV 3.0)
+assert (interp (AppC (IdC "-", [NumC 10.0; NumC 4.0])) topEnv = NumV 6.0)
+assert (interp (AppC (IdC "*", [NumC 3.0; NumC 4.0])) topEnv = NumV 12.0)
+assert (interp (AppC (IdC "/", [NumC 10.0; NumC 2.0])) topEnv = NumV 5.0)
+
+let divZeroRaised =
+    try
+        interp (AppC (IdC "/", [NumC 1.0; NumC 0.0])) topEnv |> ignore
+        false
+    with _ -> true
+
+assert divZeroRaised
+
+assert (interp (AppC (IdC "<=", [NumC 1.0; NumC 2.0])) topEnv = BoolV true)
+assert (interp (AppC (IdC "<=", [NumC 3.0; NumC 2.0])) topEnv = BoolV false)
+assert (interp (AppC (IdC "<=", [NumC 2.0; NumC 2.0])) topEnv = BoolV true)
+
+assert (interp (AppC (IdC "strlen", [StrC "hello"])) topEnv = NumV 5.0)
+assert (interp (AppC (IdC "strlen", [StrC ""])) topEnv = NumV 0.0)
+
+assert (interp (AppC (IdC "substring", [StrC "hello"; NumC 1.0; NumC 3.0])) topEnv = StrV "el")
+assert (interp (AppC (IdC "substring", [StrC "hello"; NumC 0.0; NumC 5.0])) topEnv = StrV "hello")
+assert (interp (AppC (IdC "substring", [StrC "hello"; NumC 0.0; NumC 0.0])) topEnv = StrV "")
+
+let badSubstringRaised =
+    try
+        interp (AppC (IdC "substring", [StrC "hello"; NumC 3.0; NumC 1.0])) topEnv |> ignore
+        false
+    with _ -> true
+
+assert badSubstringRaised
+
+assert (interp (AppC (IdC "equal?", [NumC 1.0; NumC 1.0])) topEnv = BoolV true)
+assert (interp (AppC (IdC "equal?", [NumC 1.0; NumC 2.0])) topEnv = BoolV false)
+assert (interp (AppC (IdC "equal?", [BoolV true |> fun _ -> IdC "true"; IdC "true"])) topEnv = BoolV true)
+assert (interp (AppC (IdC "equal?", [StrC "a"; StrC "a"])) topEnv = BoolV true)
+assert (interp (AppC (IdC "equal?", [StrC "a"; StrC "b"])) topEnv = BoolV false)
+assert (interp (AppC (IdC "equal?", [NumC 1.0; StrC "1"])) topEnv = BoolV false)
+
+assert (interp (AppC (LamC (["x"], IdC "x"), [NumC 7.0])) topEnv = NumV 7.0)
+
+let addTest =
+    AppC(
+        LamC(["x"; "y"], AppC(IdC "+", [IdC "x"; IdC "y"])),
+        [NumC 3.0; NumC 4.0]
+    )
+
+assert (interp addTest topEnv = NumV 7.0)
+
+let closureTestExpr =
+    AppC (
+        LamC (
+            ["x"],
+            AppC (
+                LamC (["f"], AppC (IdC "f", [NumC 5.0])),
+                [LamC (["y"], AppC (IdC "+", [IdC "x"; IdC "y"]))]
+            )
+        ),
+        [NumC 10.0]
+    )
+
+assert (interp closureTestExpr topEnv = NumV 15.0)
+
+let arityInterpRaised =
+    try
+        interp (AppC (LamC (["x"; "y"], IdC "x"), [NumC 1.0])) topEnv |> ignore
+        false
+    with _ -> true
+
+assert arityInterpRaised
+
+let nonFunctionRaised =
+    try
+        interp (AppC (NumC 5.0, [NumC 1.0])) topEnv |> ignore
+        false
+    with _ -> true
+
+assert nonFunctionRaised
+
+let userErrorRaised =
+    try
+        interp (AppC (IdC "error", [StrC "boom"])) topEnv |> ignore
+        false
+    with _ -> true
+
+assert userErrorRaised
 
 printfn "All tests passed."
